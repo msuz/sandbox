757374b1bc21d4ca2aca3e1d51a060669a487e44
鈴木雅彦 <m.suzuki@datumstudio.jp>
2021-11-10 01:08:44 +0900

    feature pages is in process

--- kaigo_scraping/kaigokensaku/feature_page.py
+++ kaigo_scraping/kaigokensaku/feature_page.py
@@ -1,6 +1,7 @@
 # coding: UTF-8
 
 import re
+import json
 from bs4 import BeautifulSoup
 
 class FeaturePage:
@@ -13,5 +14,58 @@ class FeaturePage:
         soup = BeautifulSoup(page_text, 'html.parser')
         if not soup.select_one('title'): return None # Error
 
-        data = {'key': 'value'} # TODO: 実装
+        data = {} # 解析結果を格納するdict
+
+        # チャート表示のためのJavaScript
+        script = soup.select_one('script:-soup-contains("AmCharts.ready")')
+
+        # <h2>タグを起点に対象データを探す
+        h2s = soup.select('article h2')
+        if not h2s: return data # empty
+        for h2 in h2s:
+            # 「お気に入り登録完了」のポップアップは無視する
+            if bool(h2.get('class')) and ('modal-title' in h2.get('class')): continue
+
+            # <h2>内のテキストをkeyにする
+            k = h2.get_text().replace('\n', ' ').strip()
+
+            # <h2>の周辺要素を元にvalueを生成する
+            n = h2.find_next() # 次要素
+            p_id = h2.find_parent().get('id') or '' # 親要素id
+            l_id = n.select_one('.legenddiv').get('id') if n.select('.legenddiv') else '' # 次要素内のグラフ要素id
+            if hasattr(FeaturePage, "parse_" + p_id): # 親要素idに対応する解析処理
+                v = getattr(FeaturePage, "parse_" + p_id)(n)
+            elif hasattr(FeaturePage, "parse_" + l_id): # チャート表示内容の解析処理
+                v = getattr(FeaturePage, "parse_" + l_id)(l_id, script)
+            else: # デフォルト：次要素のテキスト
+                v = n.get_text().replace('\n', ' ').strip() # 改行コードはスペースに
+                v = re.sub('  +', ' ', v).strip() # 余分なスペースは削除
+
+            data[k] = v # dictに追加
+
         return data
+
+    @staticmethod
+    # 「賃金改善以外で取り組んでいる処遇改善の内容」の解析処理
+    def parse_shoguuKaizenBlock(n):
+        return {'key1': 'value1', 'key2': 'value2'} # TODO
+
+    @staticmethod
+    # 「従業員の男女比」シリアルチャートの解析処理
+    def parse_legendSerialldiv_staff(l_id, script):
+        return 'value' # TODO
+
+    @staticmethod
+    # 「従業員の年齢構成」パイチャートの解析処理
+    def parse_legendPiediv_staff(l_id, script):
+        return 'value' # TODO
+
+    @staticmethod
+    # 「利用者の男女比」シリアルチャートの解析処理
+    def parse_legendSerialldiv_user(l_id, script):
+        return 'value' # TODO
+
+    @staticmethod
+    # 「利用者の年齢構成」パイチャートの解析処理
+    def parse_legendPiediv_user(l_id, script):
+        return 'value' # TODO
--- kaigo_scraping/tests/test_feature_page.py
+++ kaigo_scraping/tests/test_feature_page.py
@@ -12,5 +12,20 @@ class TestFeaturePage(TestCase):
         f.close()
         self.assertTrue('グループホームほのぼのさくら' in page_text)
         data = FeaturePage.parse(page_text)
-
-        self.assertEqual(data, {'dummy_key': 'dummy_value'})
+        self.assertEqual(data['空き人数'], '空き数/定員 0/9人 定員9人中、現在の空き数0人です。（2021年10月29日時点）')
+        self.assertEqual(data['サービスの内容に関する自由記述'], 'カラオケ・散歩と行っています・各レク・町内のお祭り参加・庭にてバーベキュー・花火大会等の実績があります')
+        self.assertEqual(data['サービスの質の向上に向けた取組'], '他の施設のサービス等を参考により良いサービスを心掛けております。')
+        self.assertEqual(data['賃金改善以外で取り組んでいる処遇改善の内容'], {'key1': 'value1', 'key2': 'value2'}) # TODO
+        self.assertEqual(data['併設されているサービス'], '利用者さんの心に寄り添う介護を心掛けてます、家族と同様な生活を心掛けています。')
+        self.assertEqual(data['保険外の利用料等に関する自由記述'], '理・美容やおむつ等は保険外としてサービスしております。')
+        self.assertEqual(data['従業員の男女比'], 'value') # TODO
+        self.assertEqual(data['従業員の年齢構成'], 'value') # TODO
+        self.assertEqual(data['従業員の特色に関する自由記述'], '当職員はベテラン者が多く介護のスキルはある方です。 ∴個々の利用者さんに合った介護ができることが当施設の特徴と思う')
+        self.assertEqual(data['利用者の男女比'], 'value') # TODO
+        self.assertEqual(data['利用者の年齢構成'], 'value') # TODO
+        self.assertEqual(data['利用者の特色に関する自由記述'], '利用者さんは皆さん仲が良く、洗濯物を干したり手伝ってくれます。')
+        self.assertEqual(data['勤務時間'], '社員の都合に合わせてシフトを組んでおります。')
+        self.assertEqual(data['賃金体系'], '周辺の施設等を参考にしております、処遇改善加算・特定処遇改善加算を取得しており職員には対応しています。')
+        self.assertEqual(data['休暇制度の内容および取得状況'], '休暇は可能な限り連続でとれるように会社として努力しています、当施設の職員は全員１００％有給消化しております。')
+        self.assertEqual(data['福利厚生の状況'], '社会保険等・懇親会など')
+        self.assertEqual(data['離職率'], '当施設は≒２０％')
