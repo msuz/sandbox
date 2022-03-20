86bd74ca30bc9d602e654097d810041fc4e3ba24
鈴木雅彦 <m.suzuki@datumstudio.jp>
2022-03-20 09:37:57 +0900

    use template

--- django_site/polls/views.py
+++ django_site/polls/views.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ','.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = Question.objects.get(pk=question_id)
+    template = loader.get_template('polls/detail.html')
+    context = {'question': question}
+    return HttpResponse(template.render(context, request))
 
 def results(request, question_id):
     response = "You're looking at the resultss of question %s."
