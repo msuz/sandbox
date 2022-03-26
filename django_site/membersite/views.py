a20865d2525bc34c18c62f205e9a385b0450b405
鈴木雅彦 <m.suzuki@datumstudio.jp>
2022-03-26 11:55:15 +0900

    [debug-toolbar.SQL] 6 queries in 1.03ms

--- django_site/membersite/views.py
+++ django_site/membersite/views.py
@@ -1,5 +1,13 @@
+from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from polls.models import Question, Choice
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = "membersite/index.html"
+
+    def get(self, request, *args, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['question_list'] = Question.objects.all()
+        return render(self.request, self.template_name, context)
+
