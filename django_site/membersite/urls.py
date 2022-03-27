0cb8b4d9b7f0bedd7e995e03e23f3c2a23533274
鈴木雅彦 <m.suzuki@datumstudio.jp>
2022-03-27 22:17:18 +0900

    use django.contrib.auth permission

--- django_site/membersite/urls.py
+++ django_site/membersite/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'membersite'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('results/', views.ResultsView.as_view(), name='results'),
 ]
--- django_site/membersite/views.py
+++ django_site/membersite/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from polls.models import Question, Choice
 import logging
 
-# Create your views here.
 class IndexView(LoginRequiredMixin, TemplateView):
-    template_name = "membersite/index.html"
-
+    template_name = 'membersite/index.html'
     def get(self, request, *args, **kwargs):
-        logging.debug("Debug Message")
-        logging.info("Info Message")
+        logging.debug('Debug Message')
+        logging.info('Info Message')
         logging.error('Error Message')
         context = super(IndexView, self).get_context_data(**kwargs)
+        context['question_list'] = Question.objects.all().prefetch_related('choices')
+        return render(self.request, self.template_name, context)
+
+class ResultsView(PermissionRequiredMixin, TemplateView):
+    template_name = 'membersite/index.html'
+    permission_required = ('polls.view_question', 'polls.view_choice')
+    def get(self, request, *args, **kwargs):
+        context = super(ResultsView, self).get_context_data(**kwargs)
         context['question_list'] = Question.objects.all().prefetch_related('choices')
         return render(self.request, self.template_name, context)
